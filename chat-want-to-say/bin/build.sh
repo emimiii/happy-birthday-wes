04be03de3bf86795fd3dd7b965205f9d9aae1d2c
Emma Lee <emmaclee06@gmail.com>
2024-01-06 20:59:44 -0500

    commented out environment switch in build

--- chat-want-to-say/bin/build.sh
+++ chat-want-to-say/bin/build.sh
@@ -1,9 +1,9 @@
 export BASE_PATH=$(cd `dirname $0`;pwd)
-if [ ! $1 ]; then
-    $1='production'
-fi
+#if [ ! $1 ]; then
+#    $1='production'
+#fi
 # Build front with yarn.
 cd $BASE_PATH/../
 yarn
-echo "yarn build --mode 'production'"
-yarn build --mode 'production'
+#echo "yarn build --mode 'production'"
+#yarn build --mode 'production'
